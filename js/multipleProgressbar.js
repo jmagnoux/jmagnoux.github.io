8aa81a40868fb5830488e42f5da2c052e3250cf4
jmagnoux <jmagnoux@gmail.com>
2017-02-14 16:02:16 +0100

    Change progress bar in iphone/ipad version to bootstrap progress bar

--- js/multipleProgressbar.js
+++ js/multipleProgressbar.js
@@ -4,57 +4,55 @@ var endColor = '#A41C22';
 window.onload = function onLoad() {
   window.index = 0;
   var progressBars=[];
-  for (var i = 0; i < $(".progressBar").length; i++) {
-    progressBars[i] = "progress"+i.toString();
-  }
-  function createBarProgress(divId){
-    var bar = new ProgressBar.Line('#'+divId, {
-      strokeWidth: 4,
-      color: startColor,
-      duration: 1400,
-      easing: 'bounce',
-      trailColor: '#414141',
-      trailWidth: 1,
-      svgStyle: {width: '100%', height: '4px'},
-      text: {
-        style: {
-          color: '#FFF',
-          position: 'absolute',
-          padding: 0,
-          margin: 0,
-          transform: null
-        },
-        autoStyleContainer: false
-      },
-      step: (state, bar) => {
-        bar.setText(Math.round(bar.value() * 100) + ' %');
-        bar.path.setAttribute('stroke', state.color);
-      }
-    });
-    var value = ($('#'+divId).attr('value') / 100);
-    bar.animate(value, {
-      from: {color: startColor},
-      to: {color: endColor}
-    });
-  }
-  function moveImg(divId)
+  var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  if(!iOS)
   {
-    var imgId = "#"+divId+"Img";
-    var value = $('#'+divId).attr('value');
-    if (value >= 1.0) {
-
-    }
-    else {
-      imgId.animate({
-        left: '+='+(Math.round(bar.value() * 100) + ' %')
-      }, {
+    function createBarProgress(divId){
+      var bar = new ProgressBar.Line('#'+divId, {
+        strokeWidth: 4,
+        color: startColor,
         duration: 1400,
-        easing: 'bounce'});
-      }
+        easing: 'bounce',
+        trailColor: '#414141',
+        trailWidth: 1,
+        svgStyle: {width: '100%', height: '4px'},
+        text: {
+          style: {
+            color: '#FFF',
+            position: 'absolute',
+            padding: 0,
+            margin: 0,
+            transform: null
+          },
+          autoStyleContainer: false
+        },
+        step: (state, bar) => {
+          bar.setText(Math.round(bar.value() * 100) + ' %');
+          bar.path.setAttribute('stroke', state.color);
+        }
+      });
+      var value = ($('#'+divId).attr('value') / 100);
+      bar.animate(value, {
+        from: {color: startColor},
+        to: {color: endColor}
+      });
+    }
+    for (var i = 0; i < $(".progressBar").length; i++) {
+      progressBars[i] = "progress"+i.toString();
+      createBarProgress(progressBars[i]);
+    }
   }
-  for (var i = 0; i < progressBars.length; i++) {
-    progressBars[i] = "progress"+i.toString();
-    createBarProgress(progressBars[i]);
-    moveImg(progressBars[i]);
+  else
+  {
+    for (var i = 0; i < $(".progressBar").length; i++) {
+      progressBars[i] = "progress"+i.toString();
+      var percentValue = $('#'+progressBars[i]).attr('value');
+      $('#'+progressBars[i]).hide();
+      $('.progress').show();
+      $('#'+progressBars[i]+'-bar').show();
+      $('#'+progressBars[i]+'-bar').attr('aria-valuenow', percentValue);
+      $('#'+progressBars[i]+'-bar').css('width', percentValue+'%');
+      $('#'+progressBars[i]+'-bar').text(percentValue+'%');
+    }
   }
 };
